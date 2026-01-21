186b48e55beb4793df6a8d05a0a4198ee3a53f25
Gokul Narayanan <gokul.narayanan@pm.me>
2026-01-21 17:12:20 +1100

    Adds error.txt to gitignore.
    Updates Scanner2 to complete implementation. Also adds support for
    C-style block comments (enclosed within /* ... */).

--- src/com/lox/Scanner2.java
+++ src/com/lox/Scanner2.java
@@ -2,6 +2,8 @@ package com.lox;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 class Scanner2 implements IScanner {
 	private int line = 1;
@@ -13,6 +15,28 @@ class Scanner2 implements IScanner {
 	private int current = 0;
 	private String source;
 
+	private static final Map<String, TokenType> keywords;
+
+	static {
+		keywords = new HashMap<>();
+		keywords.put("and", TokenType.AND);
+		keywords.put("class", TokenType.CLASS);
+		keywords.put("else", TokenType.ELSE);
+		keywords.put("false", TokenType.FALSE);
+		keywords.put("fun", TokenType.FUN);
+		keywords.put("for", TokenType.FOR);
+		keywords.put("if", TokenType.IF);
+		keywords.put("nil", TokenType.NIL);
+		keywords.put("or", TokenType.OR);
+		keywords.put("print", TokenType.PRINT);
+		keywords.put("return", TokenType.RETURN);
+		keywords.put("super", TokenType.SUPER);
+		keywords.put("this", TokenType.THIS);
+		keywords.put("true", TokenType.TRUE);
+		keywords.put("var", TokenType.VAR);
+		keywords.put("while", TokenType.WHILE);
+	}
+
 	private List<Token> tokens;
 
 	Scanner2(String source) {
@@ -21,8 +45,8 @@ class Scanner2 implements IScanner {
 	}
 
 	public List<Token> scanTokens() {
-		start = current;
 		while(!isAtEnd()) {
+			start = current;
 			scanToken();
 		}
 		tokens.add(new Token(TokenType.EOF, "", null, line));
@@ -48,11 +72,31 @@ class Scanner2 implements IScanner {
 			case '=': addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
 			case '>': addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
 			case '<': addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
+			case '/':
+				  if (match('/')) {
+					  while (peek() != '\n' && !isAtEnd()) advance();
+				  } else if (match('*')) {
+					  boolean isNestedComment =  true;
+					  while ((peek() != '*') && (peekNext() != '/') && !isAtEnd())
+						  if (advance() == '\n') ++line;
+					  if ((peek() == '*') && (peekNext() == '/')) {
+						  isNestedComment = false;
+						  advance();
+						  advance();
+					  }
+					  if (isAtEnd() & isNestedComment) {
+						  Lox.error(line, "Unescaped comment");
+					  }
+				  } else addToken(TokenType.SLASH); break;
+			case ' ':
+			case '\t':
+			case '\r': break;
+			case '\n': ++line; break;
 			case '"': string(); break;
 			default:
 				  if (isDigit(c)) {
 					  number();
-				  }else if (isAlphaNumeric(c)) {
+				  } else if (isAlpha(c)) {
 					  identifier();
 				  } else {
 					  Lox.error(line, "Invalid character.");
@@ -94,8 +138,10 @@ class Scanner2 implements IScanner {
 			advance();
 		}
 
-		if (peek() != '"' && isAtEnd())
+		if (peek() != '"' && isAtEnd()) {
 			Lox.error(line, "Unterminated string.");
+			return;
+		}
 
 		advance();
 		String literal = source.substring(start + 1, current - 1);
@@ -112,12 +158,17 @@ class Scanner2 implements IScanner {
 		if (peek() == '.' && isDigit(peekNext())) {
 			advance();
 
-			while(isDigit(peek)) advance();
+			while(isDigit(peek())) advance();
 		}
 
 		String value = source.substring(start, current);
 		double value_double = Double.parseDouble(value);
-		addToken(NUMBER, value_double);
+		addToken(TokenType.NUMBER, value_double);
+	}
+
+	private char peekNext() {
+		if (current + 1 >= source.length()) return '\0';
+		return source.charAt(current + 1);
 	}
 
 	private boolean isAlpha(char c) {
@@ -130,7 +181,13 @@ class Scanner2 implements IScanner {
 		return isAlpha(c) || isDigit(c);
 	}
 
-	private void identifer() {
-		while (isAlphaNumeric(c))
+	private void identifier() {
+		while (isAlphaNumeric(peek())) {
+			advance();
+		}
+		String text = source.substring(start, current);
+		TokenType type = keywords.get(text);
+		if (type == null) type = TokenType.IDENTIFIER;
+		addToken(type);
 	}
 }
