d64052d14cfa96b8f5e182760d3a4a4ae1f3c3a6
Gokul Narayanan <gokul.narayanan@pm.me>
2026-01-27 20:07:50 +1100

    Adds visitor pattern to expressions.
    Updates Expr classes.
    Creates an expression printer.

--- src/com/tool/GenerateAst.java
+++ src/com/tool/GenerateAst.java
@@ -6,31 +6,45 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GenerateAst {
+	private static StringBuilder indent = new StringBuilder();
+
+	private static void addIndent() {
+		indent.append("\t");
+	}
+
+	private static void removeIndent() {
+		if (indent.length() == 0) return;
+		indent.deleteCharAt(indent.length() - 1);
+	}
+
 	public static void main(String[] args) throws IOException {
-		if (args.length != 1) {
-			System.err.println("Usage: generate_ast <output directory>");
+		if (args.length != 1) { System.err.println("Usage: generate_ast <output directory>");
 			System.exit(64);
 		}
 		String outputDir = args[0];
-		defineAst(outputDir, "Expr", Arrays.asList(
-					"Binary    : Expr left, Token operator, Expr right",
-					"Grouping  : Expr expression",
-					"Literal   : Object value",
-					"Unary     : Token operator, Expr right"
-					));
+		defineAst(
+			outputDir,
+			"Expr",
+			Arrays.asList(
+			"Binary    : Expr left, Token operator, Expr right",
+			"Grouping  : Expr expression",
+			"Literal   : Object value",
+			"Unary     : Token operator, Expr right"));
 	}
 
-	public static void defineAst(
-			String outputDir,
-			String baseName,
-			List<String> types) throws IOException {
+	public static void defineAst(String outputDir, String baseName, List<String> types)
+	throws IOException {
 		String path = outputDir + "/" + baseName + ".java";
 		PrintWriter writer = new PrintWriter(path, "UTF-8");
-		writer.println("package com.lox");
-		writer.println();
-		writer.println("import java.util.list");
-		writer.println();
-		writer.println("abstract class " + baseName + " {");
+		print(writer, "package com.lox;");
+		print(writer, "");
+		print(writer, "import java.util.List;");
+		print(writer, "");
+		print(writer, "abstract class " + baseName + " {");
+
+		addIndent();
+
+		defineVisitor(writer, baseName, types);
 
 		for (String type : types) {
 			String[] typeString = type.split(":");
@@ -38,30 +52,62 @@ public class GenerateAst {
 			String fields = typeString[1].trim();
 			defineType(writer, baseName, className, fields);
 		}
-		writer.println("}");
+
+		print(writer, "abstract <R> R accept(Visitor<R> visitor);");
+		removeIndent();
+		print(writer, "}");
 		writer.close();
 	}
 
 	private static void defineType(
-			PrintWriter writer,
-			String baseName,
-			String className,
-			String fieldList) throws IOException {
-		writer.println("static class " + className + " extends " + baseName + " {");
-
-		writer.println(className + "(" + fieldList + ") {");
+	PrintWriter writer, String baseName, String className, String fieldList) throws IOException {
+		print(writer, "static class " + className + " extends " + baseName + " {");
+		addIndent();
 		String[] fields = fieldList.split(", ");
-		for (String field: fields) {
+
+		for (String field : fields) {
+			print(writer, "final " + field + ";");
+		}
+
+		print(writer, "");
+		print(writer, className + "(" + fieldList + ") {");
+		addIndent();
+		for (String field : fields) {
 			String name = field.split(" ")[1];
-			writer.println("this." + name + " = " + name + ";");
+			print(writer, "this." + name + " = " + name + ";");
 		}
+		removeIndent();
+		print(writer, "}");
 
-		writer.println("}");
+		// Visitor pattern
+		print(writer, "");
+		print(writer, "@Override");
+		print(writer, "<R> R accept(Visitor<R> visitor) {");
+		addIndent();
+		print(writer, "return visitor.visit" + className + baseName + "(this);");
+		removeIndent();
+		print(writer, "}");
 
-		for (String field: fields) {
-			writer.println("final " + field + ";");
+		removeIndent();
+		print(writer, "}");
+		print(writer, "");
+	}
+
+	private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+		print(writer, "interface Visitor<R> {");
+		addIndent();
+		for (String type: types) {
+			String[] typeString = type.split(":");
+			String typeName = typeString[0].trim();
+			print(writer, "R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
 		}
+		removeIndent();
+		print(writer, "}");
+		print(writer, "");
+	}
 
-		writer.println("}");
+	private static void print(PrintWriter writer, String str) {
+		writer.print(indent.toString());
+		writer.println(str);
 	}
 }
